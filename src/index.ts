fcb4becfd3936320f3ee5a8a080434f08f80f48a
Daniela <daniela_post@abv.bg>
2019-01-26 12:08:17 +0200

    added authentication

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,42 @@ import express from 'express';
 import listingsRouter from './api/routes/listingsRouter';
 import neighborhoodsRouter from './api/routes/neighborhoodsRouter';
 import db, { syncDatabase } from './database/Db';
+import jwt from 'express-jwt';
+import helmet = require('helmet');
+import cors = require('cors');
+const jwksRsa = require('jwks-rsa');
 
 syncDatabase(db);
 
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function(req, res, next){ 
-    res.setHeader('Last-Modified', (new Date()).toUTCString());
-    next(); 
-  });
+app.use(cors());
 
-app.use('/listings', listingsRouter);
-app.use('/neighborhoods', neighborhoodsRouter);
+app.use(function (req, res, next) {
+    res.setHeader('Last-Modified', (new Date()).toUTCString());
+    next();
+});
+
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://airbnb-stats.eu.auth0.com/.well-known/jwks.json`
+    }),
+
+    audience: 'jz1zd7BI5Lx5UjcU3Ua62XkVPqvZOuND',
+    issuer: `https://airbnb-stats.eu.auth0.com/`,
+    algorithms: ['RS256']
+});
+
+app.use('/listings', checkJwt, listingsRouter);
+app.use('/neighborhoods', checkJwt, neighborhoodsRouter);
 
 const port = process.env.PORT || 8080;
 app.listen(port);
